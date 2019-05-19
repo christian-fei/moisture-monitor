647b3126c4c363dd78810327514921b8a1ad444d
Christian Fei <crifei93@gmail.com>
2019-05-19 10:51:40 +0200

    fix raspi-io

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var five = require('johnny-five')
-var Raspi = require('raspi-io')
-var board = new five.Board({
+const five = require('johnny-five')
+const Raspi = require('raspi-io').RaspiIO
+const board = new five.Board({
   io: new Raspi()
 })
 
 board.on('ready', function () {
   console.log('board ready')
-  var led = new five.Led('P1-13')
+  const led = new five.Led('P1-13')
   led.blink()
 })
